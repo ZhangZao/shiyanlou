68ebf9472c1c486347c24e2910467886f758afa1
zhangzao <384833504@qq.com>
2018-10-25 10:42:10 +0800

    app update

--- news/app.py
+++ news/app.py
@@ -1,10 +1,37 @@
 #!/usr/bin/env python3
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, abort
 import os
 import json
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root@localhost/news'
+db = SQLAlchemy(app)
 JSON_DIR = '../files'
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    created_time = db.Column(db.DateTime)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship('Category', backref=db.backref('posts'\
+            , lazy = 'dynamic'))
+    content = db.Column(db.Text)
+
+    def __init__(self, title, created_time, category, content):
+        
+        self.created_time = created_time 
+        self.title = title
+        self.category = category
+        self.content = content
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+
+    def __init__(self, name):
+        self.name = name
 
 def read_file():
     titles=[]
@@ -18,19 +45,20 @@ def read_file():
 
 @app.route('/')
 def index():
-    titles = read_file()
+    titles = {}
+    files = File.query.all()
+    for file in files:
+        titles[file.id]=file.title
     return render_template('index.html', titles=titles)
 
-@app.route('/files/<filename>')
-def file(filename):
-    data = {}
-    json_path = os.path.join(JSON_DIR, '{}.json'.format(filename))
-    if not os.path.isfile(json_path):
-        abort(404)
-    else:
-        with open(json_path) as f:
-            data = json.load(f)
-    return render_template('file.html', data = data)
+@app.route('/files/<file_id>')
+def file(file_id):
+    the_file = File.query.filter_by(id = file_id).first()
+    title = the_file.title
+    category = the_file.category
+    created_time = the_file.created_time
+    content = the_file.content
+    return render_template('file.html', title=title, category=category, created_time=created_time, content=content)
 
 @app.errorhandler(404)
 def not_found(err):
